ac7eb8c9108d8a82d2934e14ebccf2f6772c3da9
yadavprakash <yadavprakash.it@gmail.com>
2021-05-31 15:06:53 +0530

    add pre-commit

--- main.tf
+++ main.tf
@@ -63,7 +63,7 @@ resource "aws_flow_log" "vpc_flow_log" {
   log_destination      = var.s3_bucket_arn
   log_destination_type = "s3"
   traffic_type         = var.traffic_type
-vpc_id               = join("", aws_vpc.default.*.id)
+  vpc_id               = join("", aws_vpc.default.*.id)
   tags                 = module.labels.tags
 }
 
