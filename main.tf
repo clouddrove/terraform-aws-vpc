7bdf2e9bc64b96c63c1f33dbedc7554bfd914e6e
vibhuti goyal <vibhutigoyal.vg78@gmail.com>
2023-05-30 16:59:34 +0530

    bug: removed deprecated arguments

--- main.tf
+++ main.tf
@@ -20,14 +20,12 @@ module "labels" {
 #Module      : VPC
 #Description : Terraform module to create VPC resource on AWS.
 resource "aws_vpc" "default" {
-  count = var.vpc_enabled ? 1 : 0
+  count = var.vpc_enabled == true ? 1 : 0
 
   cidr_block                       = var.cidr_block
   instance_tenancy                 = var.instance_tenancy
   enable_dns_hostnames             = var.enable_dns_hostnames
   enable_dns_support               = var.enable_dns_support
-  enable_classiclink               = var.enable_classiclink
-  enable_classiclink_dns_support   = var.enable_classiclink_dns_support
   ipv4_ipam_pool_id                = var.ipv4_ipam_pool_id
   ipv4_netmask_length              = var.ipv4_ipam_pool_id != "" ? var.ipv4_netmask_length : null
   assign_generated_ipv6_cidr_block = true
