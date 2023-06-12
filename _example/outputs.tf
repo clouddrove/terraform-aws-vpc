e968bd2c3e8cc8d22d1d0f4709dfb0a851154724
vibhuti goyal <vibhutigoyal.vg78@gmail.com>
2023-06-12 13:21:34 +0530

    updated vpc module

--- _example/outputs.tf
+++ _example/outputs.tf
@@ -2,7 +2,6 @@ output "id" {
   value       = module.vpc.*.vpc_id
   description = "The ID of the VPC."
 }
-
 output "tags" {
   value       = module.vpc.*.tags
   description = "A mapping of tags to assign to the resource."
@@ -11,17 +10,14 @@ output "vpc_cidr" {
   value = module.vpc.*.vpc_cidr_block
    description = "The primary IPv4 CIDR block"
 }
-
 output "vpc_ipv6_cidr_block" {
   value       = module.vpc.*.ipv6_cidr_block
   description = "The primary IPv6 CIDR block"
 }
-
 output "vpc_ipv6_association_id" {
   value       = module.vpc.*.vpc_ipv6_association_id
   description = "The association ID for the primary IPv6 CIDR block"
 }
-
 output "ipv6_cidr_block_network_border_group" {
   value       = module.vpc.*.ipv6_cidr_block_network_border_group
   description = "The Network Border Group Zone name"
