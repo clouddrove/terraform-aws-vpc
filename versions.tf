be7a6eee7aae73e0e4611e4d345a59d19b1ca6fd
anmolnagpal <anmol@clouddrove.com>
2023-07-24 19:22:55 +0530

    update module with latest changes

--- versions.tf
+++ versions.tf
@@ -1,11 +1,11 @@
-//# Terraform version
-//terraform {
-//  required_version = ">= 1.4.6"
-//
-//  required_providers {
-//    aws = {
-//      source  = "hashicorp/aws"
-//      version = ">= 5.1.0"
-//    }
-//  }
-//}
+# Terraform version
+terraform {
+  required_version = ">= 1.4.6"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.1.0"
+    }
+  }
+}
