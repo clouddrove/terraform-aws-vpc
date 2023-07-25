b59d7606d93713fafeb9dfe194c7110351787183
Archit Chopra <chopra13archit@gmail.com>
2023-07-25 15:46:23 +0530

    fix: Fixed _example/complete and dependabot.yml

--- _example/complete/example.tf
+++ _example/complete/example.tf
@@ -5,14 +5,13 @@ provider "aws" {
 ## VPC Module Call. 
 ##-----------------------------------------------------------------------------
 module "vpc" {
-  source                    = "../.."
-  name                      = "vpc"
-  environment               = "example"
-  cidr_block                = "10.0.0.0/16"
-  enable_flow_log           = true
-  flow_log_destination_type = "s3"                      # Comment or remove when flow log destination is set "cloud-watch-logs"
-  flow_logs_bucket_name     = "gc-vpc-flow-logs-bucket" # Comment or remove when flow log destination is set "cloud-watch-logs"
-  # create_flow_log_cloudwatch_iam_role = true # To be uncomment when flow log destination is set "cloud-watch-logs"
+  source                           = "../.."
+  name                             = "vpc"
+  environment                      = "example"
+  cidr_block                       = "10.0.0.0/16"
+  enable_flow_log                  = true
+  flow_log_destination_type        = "s3"
+  flow_logs_bucket_name            = "gc-vpc-flow-logs-bucket"
   additional_cidr_block            = ["172.3.0.0/16", "172.2.0.0/16"]
   dhcp_options_domain_name         = "service.consul"
   dhcp_options_domain_name_servers = ["127.0.0.1", "10.10.0.2"]
