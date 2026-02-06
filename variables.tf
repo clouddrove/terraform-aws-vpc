0d111b6445ec2bfc76dc12d9d5d1f4d33f108712
Sunny-Mor <sunnymor830@gmail.com>
2026-02-06 21:23:59 +0530

    feat: Add extra tags variable and PR check workflow

--- variables.tf
+++ variables.tf
@@ -6,6 +6,12 @@ variable "name" {
   description = "Name  (e.g. `app` or `cluster`)."
 }
 
+variable "extra_tags" {
+  type        = map(string)
+  default     = {}
+  description = "Additional tags (e.g. map(`BusinessUnit`,`XYZ`)."
+}
+
 variable "environment" {
   type        = string
   default     = ""
